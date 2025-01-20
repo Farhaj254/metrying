f8bf362a1972f3eb4c6f75cd190352e3c9df2d8e
Farhaj254 <xsour1254@gmail.com>
2025-01-20 16:01:26 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,24 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker installed');
+const cacheName = "game-hub-cache-v1";
+const filesToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/manifest.json",
+  "/images/logo-192x192.png",
+  "/images/logo-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    console.log('Fetch intercepted for:', event.request.url);
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });

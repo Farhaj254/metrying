73ef448ad670623e9e18c922f4891e3747d38491
Farhaj254 <xsour1254@gmail.com>
2025-01-20 16:19:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -175,7 +175,7 @@ function addShortcut() {
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("service-worker.js")
+    .register('/service-worker.js')
     .then(() => console.log("Service Worker Registered"))
     .catch((err) => console.error("Service Worker Registration Failed", err));
 }

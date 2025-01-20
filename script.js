311bc0c45bcff82694e7c1e00efbbc6588b8659a
Farhaj254 <xsour1254@gmail.com>
2025-01-20 15:51:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -172,3 +172,14 @@ function addShortcut() {
         alert('Shortcut feature is not available on this browser.');
     }
 }
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+            console.error('Service Worker registration failed:', error);
+        });
+}
+
